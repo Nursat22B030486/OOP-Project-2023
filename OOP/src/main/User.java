93a3fa0b6364925e60aac81b2b59cf8a8a1c75b2
n_kordabay <kuba.kordabai.08@mail.ru>
2023-12-14 11:11:41 +0600

    realization user

--- OOP/src/main/User.java
+++ OOP/src/main/User.java
@@ -1,5 +1,62 @@
 package main;
 
 public abstract class User {
+	protected String username;
+	protected String password;
+	protected String userId;
+	protected UserPersonalInfo infoUser;
+	protected boolean active = true;
+	
+	public User(String username, String password, String userId, UserPersonalInfo infoUser) {
+		this.username = username;
+		this.password = password;
+		this.userId = userId;
+		this.infoUser = infoUser;
+	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void changeUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	public void changePassword(String password) {
+		this.password = password;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	
+	public UserPersonalInfo getInfoUser() {
+		return infoUser;
+	}
+	public void updateInfoUser(UserPersonalInfo infoUser) {
+		this.infoUser = infoUser;
+	}
+	
+	public boolean logIn(String username, String password) {
+		if(this.username.equals(username) && this.password.equals(password)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean logOut() {
+		if(this.active == true) {
+			this.active = false;
+			return true;
+		}
+		return false;
+	}
+	
+	public void viewNews() {
+		for(News n: SystemData.news) {
+			System.out.println(news);
+		}
+	}
 }
